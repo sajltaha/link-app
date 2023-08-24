0663d12de4e87247686808dc748f3baa96b525ee
sajltaha <s.t.a.r.n.k06@gmail.com>
2023-08-25 03:02:17 +0600

    last update

--- src/components/AppAction/AppAction.js
+++ src/components/AppAction/AppAction.js
@@ -1,10 +1,12 @@
 import { useState } from "react";
 import Container from "../Container/Container";
 import style from "./AppAction.module.css";
+import { BiTransfer } from "react-icons/bi";
 
 export default function AppAction() {
   const [inputValue, setInputValue] = useState("");
   const [isActive, setIsActive] = useState(false);
+  const [finalLink, setFinalLink] = useState("");
 
   const inputChangeHandler = (e) => {
     setInputValue(e.target.value);
@@ -12,13 +14,18 @@ export default function AppAction() {
 
   async function clickChange() {
     if (inputValue) {
-      const res = await fetch(`https://clck.ru/--?url=${inputValue}`)
-      const link = await res.text()
-      console.log(link)
+      const res = await fetch(`https://clck.ru/--?url=${inputValue}`);
+      const link = await res.text();
+      setFinalLink(link);
+      setInputValue('')
       setIsActive(true);
     } else {
       setIsActive(false);
     }
+  }
+
+  const copyLink = () => {
+    navigator.clipboard.writeText(finalLink);
   };
 
   return (
@@ -45,7 +52,22 @@ export default function AppAction() {
         </div>
       </Container>
       <Container>
-        <div style={{ display: isActive ? "block" : "none" }}>asdas</div>
+        <div
+          style={{ display: isActive ? "flex" : "none" }}
+          className={style.result_window}
+        >
+          <a
+            className={style.result_window_link}
+            href={finalLink}
+            target="_blank"
+          >
+            <BiTransfer />
+            <span>{finalLink.slice(8)}</span>
+          </a>
+          <button className={style.result_window_clipboard} onClick={copyLink}>
+            Копировать
+          </button>
+        </div>
       </Container>
     </>
   );
